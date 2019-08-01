a5f9d7e56abdaf6f6e0f2696ed8cfc3d430af585
hiulit <hiulit@gmail.com>
2019-08-01 12:06:35 +0200

    Rename file

---
+++ index.js
@@ -0,0 +1,80 @@
+const cors = require('cors')
+const fetch = require('node-fetch')
+const express = require('express')
+
+const { endpoint, port } = require('./config')
+
+const app = express()
+
+app.set('port', port)
+app.use(cors())
+
+async function getGames () {
+  const response = await fetch(endpoint)
+  return response.json()
+}
+
+app.get('/', (req, res) => {
+  res.redirect('/api')
+})
+
+app.get('/api', (req, res) => {
+  getGames().then(games => {
+    res.json(games)
+  })
+})
+
+app.get('/api/authors', (req, res) => {
+  getGames().then(games => {
+    let authors = []
+
+    for (let i = 0; i < games.length; i++) {
+      const game = games[i]
+
+      if (!authors.includes(game.author)) {
+        authors.push(game.author)
+      }
+    }
+    res.json(authors)
+  })
+})
+
+app.get('/api/author/:author', function (req, res) {
+  getGames().then(games => {
+    let gamesByAuthor = []
+
+    for (let i = 0; i < games.length; i++) {
+      const game = games[i]
+
+      if (game.author.toUpperCase() === req.params.author.toUpperCase()) {
+        gamesByAuthor.push(game)
+      }
+    }
+    res.json(gamesByAuthor)
+  })
+})
+
+app.get('/api/platform/:platform', function (req, res) {
+  getGames().then(games => {
+    let gamesByPlatforms = []
+
+    for (let i = 0; i < games.length; i++) {
+      const game = games[i]
+
+      if (game.platforms.length) {
+        for (let j = 0; j < game.platforms.length; j++) {
+          const platform = game.platforms[j]
+
+          if (platform.toUpperCase() === req.params.platform.toUpperCase()) {
+            gamesByPlatforms.push(game)
+          }
+        }
+      }
+    }
+    res.json(gamesByPlatforms)
+  })
+})
+
+app.listen(port, () => {
+  console.log('Server is running at http://localhost:' + port)
+})
