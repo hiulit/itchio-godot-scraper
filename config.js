9fba5f4a7fc21e4500f44e624dfe102e6eb37b09
hiulit <hiulit@gmail.com>
2019-08-05 12:08:45 +0200

    Remove .env, config.js and dotenv package

--- config.js
+++
@@ -1,9 +0,0 @@
-// const dotenv = require('dotenv') // We use 'nodemon -r dotenv/config index.js' instead.
-
-// dotenv.config() // We use 'nodemon -r dotenv/config index.js' instead.
-
-module.exports = {
-  scraperUrl: process.env.SCRAPER_URL,
-  apiUrl: process.env.API_URL,
-  port: process.env.PORT
-}
