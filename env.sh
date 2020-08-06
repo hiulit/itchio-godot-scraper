6750f9e9f9d3341dda59c9246e2dcdd47c73e5c7
hiulit <hiulit@gmail.com>
2020-08-06 17:14:54 +0200

    Fixed some paths

---
+++ env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is an example environment variables file.
+# To use it, add a dot '.' at the beginning of this file.
+# (e.g. ".env.sh")
+
+# The full path to the 'update-games-job.sh' path.
+ENV_SCRIPT_PATH=""
+# The full path to your SSH private key.
+ENV_SSH_PRIVATE_KEY_PATH=""
